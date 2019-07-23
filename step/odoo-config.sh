6abf7699c33a850b7d0d3e3fa888ba28038e61e9
ejabu <muhammadfahreza@gmail.com>
2019-07-23 13:09:17 +0700

    edit : config

--- step/odoo-config.sh
+++ step/odoo-config.sh
@@ -6,14 +6,14 @@
 
 CONFIG_FILE="$ODOO_CONFIG_DIRECTORY/$ODOO_CONFIG_FILE.conf"
 echo -e "Create Config File in $CONFIG_FILE"
-sudo touch $CONFIG_FILE
+touch $CONFIG_FILE
 
 echo -e "Create Content"
-sudo su root -c "printf '[options] \n' >> $CONFIG_FILE"
-sudo su root -c "printf '# Setiap config file untuk Odoo memerlukan [options] di awal \n' >> $CONFIG_FILE"
-sudo su root -c "printf 'admin_passwd = ${OE_SUPERADMIN}\n' >> $CONFIG_FILE"
-sudo su root -c "printf 'xmlrpc_port = ${OE_PORT}\n' >> $CONFIG_FILE"
-sudo su root -c "printf '# addons_path = ${ODOO_DEVELOPMENT_DIRECTORY}/ark-dev/basic,${ODOO_DEVELOPMENT_DIRECTORY}/ark-dev/intermediate\n' >> $CONFIG_FILE"
+printf '[options] \n' >> $CONFIG_FILE
+printf '# Setiap config file untuk Odoo memerlukan [options] di awal \n' >> $CONFIG_FILE
+printf 'admin_passwd = ${OE_SUPERADMIN}\n' >> $CONFIG_FILE
+printf 'xmlrpc_port = ${OE_PORT}\n' >> $CONFIG_FILE
+printf '# addons_path = ${ODOO_DEVELOPMENT_DIRECTORY}/ark-dev/basic,${ODOO_DEVELOPMENT_DIRECTORY}/ark-dev/intermediate\n' >> $CONFIG_FILE"
 
 echo -e "Changing Permission"
 sudo chown $THIS_USER:$THIS_USER $CONFIG_FILE
