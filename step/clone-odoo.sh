7db9a9b487e3416c24ff0b8583e3d15b8ec95972
ejabu <muhammadfahreza@gmail.com>
2019-07-23 10:12:59 +0700

    edit

--- step/clone-odoo.sh
+++ step/clone-odoo.sh
@@ -8,3 +8,7 @@ sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $
 
 sudo chown -R $THIS_USER:$THIS_USER $ODOO_SOURCE_DIRECTORY/*
 sudo chmod 775 -R $THIS_USER:$THIS_USER $ODOO_SOURCE_DIRECTORY/*
+
+echo -e "\n==== Installing ODOO Requirements ===="
+
+sudo pip install -r $ODOO_SOURCE_DIRECTORY/$OE_VERSION/requirements.txt
